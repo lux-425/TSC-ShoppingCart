9b9a0584d57068a7f4ad9c025fe6dabd6533b622
Luke_425 <garcia.luc66@gmail.com>
2022-10-18 18:41:06 +0200

    Setup and basic routing

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import App from './App'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import { BrowserRouter } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
